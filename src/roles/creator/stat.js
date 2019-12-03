61caea4e7144a9fb4611956b36df2bf0c3ae34a4
harold ho <harold.ho@oneshop.team>
2019-12-03 12:01:00 +0800

    Add examples

--- src/roles/creator/stat.js
+++ src/roles/creator/stat.js
@@ -11,24 +11,48 @@ class Stat extends Role {
         };
         /**
          * Retrieve most engaged hours of your articles
+         * @param {Object} query 
+         * @param {String} query[page]
+         * 
+         * Examples:
+         *  os.creator.stat.peak.get({page:1})
+         * 
          */
         this.peak = {
             get : (query) => this.request.get(`${this.baseURL}/peaks`, query || {}, this.credentials)
         };
         /**
          * Retrieve best sellers stats
+         * @param {Object} query 
+         * @param {String} query[page]
+         * 
+         * Examples:
+         *  os.creator.stat.sale.get({page:1})
+         * 
          */
         this.sale = {
             get : (query) => this.request.get(`${this.baseURL}/sales`, query || {}, this.credentials)
         };
         /**
          * Retrieve the classification details of your articles
+         * @param {Object} query 
+         * @param {String} query[page]
+         * 
+         * Examples:
+         *  os.creator.stat.strength.get({page:1})
+         * 
          */
         this.strength = {
             get : (query) => this.request.get(`${this.baseURL}/strengths`, query || {}, this.credentials)
         };
         /**
          * Retrieve the view info of your articles
+         * @param {Object} query 
+         * @param {String} query[page]
+         * 
+         * Examples:
+         *  os.creator.stat.view.get({page:1})
+         * 
          */
         this.view = {
             get : (query) => this.request.get(`${this.baseURL}/views`, query || {}, this.credentials)
